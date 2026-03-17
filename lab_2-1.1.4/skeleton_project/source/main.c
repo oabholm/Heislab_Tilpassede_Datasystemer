dc5f70791baad9a9dd0a4d22a3993f2225f9e2bc
jenny11penny <jennygrindheim@gmail.com>
2026-03-17 14:32:09 +0100

    nyeste

---
+++ lab_2-1.1.4/skeleton_project/source/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include "driver/elevio.h"
+#include "functions.h"
+
+
+
+int main(){
+    elevio_init();
+    
+    printf("=== Example Program ===\n");
+
+    initialize();
+        
+    while(1){
+        int floor = elevio_floorSensor();
+
+
+        for(int f = 0; f < N_FLOORS; f++){
+            for(int b = 0; b < N_BUTTONS; b++){
+                if (elevio_callButton(f,b)) {
+                    addOrder(f,b);
+                }
+            }
+        }
+        
+        checkStop(floor);
+        moveElevator(floor);
+        
+
+        if(elevio_stopButton()){
+            stopButton(floor);
+        }
+
+        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
+    }
+
+    return 0;
+    
+}
